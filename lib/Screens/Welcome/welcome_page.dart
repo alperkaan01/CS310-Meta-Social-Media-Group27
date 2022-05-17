6fc3da124530d333a23e706ad0f3d78de3108391
alperkaan01 <88709260+alperkaan01@users.noreply.github.com>
2022-05-17 21:24:58 +0300

    init commit

---
+++ lib/Screens/Welcome/welcome_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
+
+//This page consist of the navigators for login and Sign-up page
