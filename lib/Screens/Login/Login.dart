a8b250b0c1780ebd1aceaae92d6aaa5bc5d56b8f
alperkaan01 <88709260+alperkaan01@users.noreply.github.com>
2022-05-19 21:57:56 +0300

    Update Login.dart

--- lib/Screens/Login/Login.dart
+++ lib/Screens/Login/Login.dart
@@ -1,4 +1,5 @@
 import 'package:cs310_mainproject/Screens/HomePage/Homepage.dart';
+import 'package:cs310_mainproject/Screens/SignUp/Signup.dart';
 import 'package:cs310_mainproject/colors.dart' as color;
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -18,183 +19,194 @@ class _LoginState extends State<Login> {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Column(
-              children: [
-                Stack(
-                  children:[
-                    Positioned(
-                      child: Container(
-                        width: size.width,
-                        height: 400,
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [
-                              color.AppColor.gradientFirst.withOpacity(0.5),
-                              color.AppColor.gradientSecond.withOpacity(0.8),
-                              color.AppColor.SecondMainColor.withOpacity(0.7),
-                            ],
-                          ),
-                          borderRadius: BorderRadius.only(
-                            bottomRight: Radius.circular(100),
-                            bottomLeft: Radius.circular(100),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Positioned(child: Container(
-                      child: Lottie.network("https://assets1.lottiefiles.com/packages/lf20_jcikwtux.json"),
-                    )),
-                  ],
-                ),
-                Stack(
-                  children: [
-                    Positioned(child: Container(
-                      height: 270,
-                      width: size.width,
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(300),
-                        ),
-                      ),
-                    )),
-                    Positioned(child: Container(
-                      height: 270,
-                      width: size.width,
-                      decoration: BoxDecoration(
-                        color: Colors.lightBlue.withOpacity(0.4),
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(500),
-                        ),
-                      ),
-                    )),
-                    Positioned(child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                          child: Container(
-                            margin: const EdgeInsets.only(top: 10,),
-                            height: 50,
-                            decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                colors: [
-                                  color.AppColor.gradientFirst.withOpacity(0.5),
-                                  color.AppColor.gradientSecond.withOpacity(0.8),
-                                  color.AppColor.SecondMainColor.withOpacity(0.7),
-                                ],
-                              ),
-                              borderRadius: BorderRadius.circular(50),
-                            ),
-                            child: const Padding(
-                              padding: EdgeInsets.only(left: 25,),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: "E-mail",
-                                  hintStyle: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 10,),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                          child: Container(
-                            height: 50,
-                            decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                colors: [
-                                  color.AppColor.gradientFirst.withOpacity(0.5),
-                                  color.AppColor.gradientSecond.withOpacity(0.8),
-                                  color.AppColor.SecondMainColor.withOpacity(0.7),
-                                ],
-                              ),
-                              borderRadius: BorderRadius.circular(50),
-                            ),
-                            child: const Padding(
-                              padding: EdgeInsets.only(left: 25,),
-                              child: TextField(
-                                obscureText: true,
-
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: "Password",
-                                  hintStyle: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Container(
-                          margin: const EdgeInsets.only(top: 10,),
-                          width: size.width - 50,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Column(
+                children: [
+                  Stack(
+                    children:[
+                      Positioned(
+                        child: Container(
+                          width: size.width,
+                          height: 400,
                           decoration: BoxDecoration(
-                            color: color.AppColor.gradientFirst.withOpacity(0.9),
-                            borderRadius: BorderRadius.circular(40),
-                          ),
-                          child: FlatButton(
-                            onPressed: (){
-                              //Navigator push homepage
-                              Navigator.push(context, MaterialPageRoute(builder: (context){
-                                return HomePage();
-                              }));
-                            },
-                            child: Text("Login", style: GoogleFonts.bebasNeue(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500,
-                            ),),
+                            gradient: LinearGradient(
+                              colors: [
+                                color.AppColor.gradientFirst.withOpacity(0.5),
+                                color.AppColor.gradientSecond.withOpacity(0.8),
+                                color.AppColor.SecondMainColor.withOpacity(0.7),
+                              ],
+                            ),
+                            borderRadius: const BorderRadius.only(
+                              bottomRight: Radius.circular(100),
+                              bottomLeft: Radius.circular(100),
+                            ),
                           ),
                         ),
-                        const SizedBox(height: 10,),
-                        Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 100,),
-                          child: Row(
-                            children: const [
-                              Text("Not a member yet?"),
-                              SizedBox(width: 5,),
-                              Text("Register Now", style: TextStyle(
-                                color: Colors.blueAccent,
-                                fontWeight: FontWeight.bold,
-                              ),)
-                            ],
+                      ),
+                      Positioned(child: Container(
+                        child: Lottie.network("https://assets1.lottiefiles.com/packages/lf20_jcikwtux.json"),
+                      )),
+                    ],
+                  ),
+                  Stack(
+                    children: [
+                      Positioned(child: Container(
+                        height: 270,
+                        width: size.width,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(300),
                           ),
                         ),
-                        SizedBox(height: 10,),
-                        Container(
-                          padding: const EdgeInsets.only(left: 135,),
-                          child: Row(
-                            children: [
-                              const Icon(Icons.facebook, size: 45, color: Colors.blueAccent,),
-                              const SizedBox(width: 15,),
-                              Container(
-                                padding: const EdgeInsets.all(10),
-                                decoration: BoxDecoration(
-                                  color: Colors.red,
-                                  borderRadius: BorderRadius.circular(100),
+                      )),
+                      Positioned(child: Container(
+                        height: 270,
+                        width: size.width,
+                        decoration: BoxDecoration(
+                          color: Colors.lightBlue.withOpacity(0.4),
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(500),
+                          ),
+                        ),
+                      )),
+                      Positioned(child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                            child: Container(
+                              margin: const EdgeInsets.only(top: 10,),
+                              height: 50,
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [
+                                    color.AppColor.gradientFirst.withOpacity(0.5),
+                                    color.AppColor.gradientSecond.withOpacity(0.8),
+                                    color.AppColor.SecondMainColor.withOpacity(0.7),
+                                  ],
                                 ),
-                                child: const Icon(FontAwesomeIcons.google, size: 18, color: Colors.white,),
+                                borderRadius: BorderRadius.circular(50),
                               ),
-                              const SizedBox(width: 15,),
-                              const Icon(FontAwesomeIcons.twitterSquare, size: 37, color: Colors.blueAccent,),
-                            ],
+                              child: const Padding(
+                                padding: EdgeInsets.only(left: 25,),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "E-mail",
+                                    hintStyle: TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
                           ),
-                        )
-                      ],
-                    )),
-                  ],
-                ),
-              ],
-            ),
-          ],
+                          const SizedBox(height: 10,),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                            child: Container(
+                              height: 50,
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [
+                                    color.AppColor.gradientFirst.withOpacity(0.5),
+                                    color.AppColor.gradientSecond.withOpacity(0.8),
+                                    color.AppColor.SecondMainColor.withOpacity(0.7),
+                                  ],
+                                ),
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              child: const Padding(
+                                padding: EdgeInsets.only(left: 25,),
+                                child: TextField(
+                                  obscureText: true,
+
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Password",
+                                    hintStyle: TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(top: 10,),
+                            width: size.width - 50,
+                            decoration: BoxDecoration(
+                              color: color.AppColor.gradientFirst.withOpacity(0.9),
+                              borderRadius: BorderRadius.circular(40),
+                            ),
+                            child: FlatButton(
+                              onPressed: (){
+                                //Navigator push homepage
+                                Navigator.push(context, MaterialPageRoute(builder: (context){
+                                  return HomePage();
+                                }));
+                              },
+                              child: Text("Login", style: GoogleFonts.bebasNeue(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                              ),),
+                            ),
+                          ),
+                          Container(
+                            //color: Colors.black,
+                            width: size.width,
+                            padding: const EdgeInsets.symmetric(horizontal: 100,),
+                            child: Row(
+                              children: [
+                                const Text("Not a member yet?"),
+                                Container(
+                                  width: MediaQuery.of(context).size.width - 320,
+                                  child: FlatButton(
+                                    onPressed: (){
+                                      Navigator.push(context, MaterialPageRoute(builder: (context){
+                                        return SignUp();
+                                      }));
+                                    },
+                                    child: const Text("Register Now", style: TextStyle(
+                                      color: Colors.blueAccent,
+                                      fontWeight: FontWeight.bold,
+                                    ),),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.only(left: 135,),
+                            child: Row(
+                              children: [
+                                const Icon(Icons.facebook, size: 45, color: Colors.blueAccent,),
+                                const SizedBox(width: 15,),
+                                Container(
+                                  padding: const EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    color: Colors.red,
+                                    borderRadius: BorderRadius.circular(100),
+                                  ),
+                                  child: const Icon(FontAwesomeIcons.google, size: 18, color: Colors.white,),
+                                ),
+                                const SizedBox(width: 15,),
+                                const Icon(FontAwesomeIcons.twitterSquare, size: 37, color: Colors.blueAccent,),
+                              ],
+                            ),
+                          )
+                        ],
+                      )),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
