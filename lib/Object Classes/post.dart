4835322b97c99adea561424d4e3ecea6706f162e
alperkaan01 <88709260+alperkaan01@users.noreply.github.com>
2022-05-27 18:39:26 +0300

    Create post.dart

---
+++ lib/Object Classes/post.dart
@@ -0,0 +1,15 @@
+class Post {
+  String URL;
+  String text;
+  String date;
+  int likes;
+  int comments;
+
+  Post({
+    required this.URL,
+    required this.text,
+    required this.date,
+    required this.likes,
+    required this.comments,
+  });
+}
