f03681d9633382d8b57a6d79db16ae780f4e8d9c
alivehbi <101273047+alivehbi@users.noreply.github.com>
2022-05-30 21:01:45 +0300

    Create nft.dart

---
+++ lib/Object Classes/nft.dart
@@ -0,0 +1,15 @@
+class NFT {
+  String URL;
+  String text;
+  String date;
+  int likes;
+  int money;
+
+  NFT({
+    required this.URL,
+    required this.text,
+    required this.date,
+    required this.likes,
+    required this.money,
+  });
+}
