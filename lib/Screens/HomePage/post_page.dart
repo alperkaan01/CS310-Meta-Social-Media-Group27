1330cb2ab0b37ae55c9ebf803df31b41531ca52a
alivehbi <101273047+alivehbi@users.noreply.github.com>
2022-06-01 13:33:56 +0300

    Update post_page.dart

--- lib/Screens/HomePage/post_page.dart
+++ lib/Screens/HomePage/post_page.dart
@@ -1,7 +1,11 @@
 import 'package:cs310_mainproject/Screens/Login/Login.dart';
+import 'package:cs310_mainproject/Screens/Welcome/welcome_page.dart';
+import 'package:cs310_mainproject/UI/postcard.dart';
 import 'package:flutter/material.dart';
 
+import '../../Object Classes/auth.dart';
 import '../../Object Classes/colors.dart';
+import '../../Object Classes/post.dart';
 
 class PostPage extends StatefulWidget {
   const PostPage({Key? key}) : super(key: key);
@@ -11,26 +15,70 @@ class PostPage extends StatefulWidget {
 }
 
 class _PostPageState extends State<PostPage> {
+  List<Post> posts = [
+    Post( URL: "https://www.animalfriends.co.uk/siteassets/media/images/article-images/cat-articles/38_afi_article1_caring-for-a-kitten-tips-for-the-first-month.png",text: 'Hello MetaWorld 1', date: 'March 31', likes: 10, comments: 0,UserURL: 'https://support.signal.org/hc/article_attachments/360083910451/animated-2.gif' ,username: 'User62'),
+    Post( URL: "https://kitcheninred.com/wp-content/uploads/2020/06/%C3%A7ikolatal%C4%B1-pasta-1-scaled.jpg",text: 'Hello MetaWorld 2', date: 'March 30', likes: 0, comments: 5,UserURL: 'https://support.signal.org/hc/article_attachments/360083910451/animated-2.gif' ,username: 'User62'),
+    Post( URL: "https://i.pinimg.com/originals/62/f1/65/62f165cebd814ec81f1e5a324eecbdd1.jpg", text: 'Hello MetaWorld 3', date: 'March 29', likes: 20, comments: 10,UserURL: 'https://support.signal.org/hc/article_attachments/360083910451/animated-2.gif' ,username: 'User62'),
+  ];
+  void deletePost(Post post) {
+    setState(() {
+      posts.remove(post);
+    });
+  }
+  void LikeIncrement(Post post) {
+    setState(() {
+      post.likes++;
+    });
+  }
+  int currentColor = 0;
+  final colors = [
+    Colors.greenAccent,
+    Colors.lightBlueAccent,
+    AppColor.WelcomeBackground,
+    AppColor.SecondMainColor,
+    Colors.red,
+    Colors.yellow,
+
+  ];
+  void colorChange() {
+    setState(() {
+      if (currentColor < 5) {
+        currentColor = currentColor + 1;
+      }
+      else {
+        currentColor = 0;
+      }
+    });
+  }
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    final AuthService _auth = AuthService();
 
     return SingleChildScrollView(
         child: Column(
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
 
               children: [
                 Container(
                   padding: const EdgeInsets.only(top: 25, left: 10,),
-                  child: FloatingActionButton(onPressed: (){
+                  child: FloatingActionButton(onPressed: () async {
+                    await _auth.signOut();
                     Navigator.pop(context, MaterialPageRoute(builder: (context){
-                      return const Login();
+                      return WelcomeScreen();
                     }));
                   },
-                    backgroundColor: AppColor.WelcomeBackground,
+                    backgroundColor: colors[currentColor],
                     child: const Icon(Icons.logout,color: Colors.black,)
                     ,),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 25, right: 10,),
+                  child: FloatingActionButton(onPressed: colorChange,
+                    backgroundColor: colors[currentColor],
+                    child: const Icon(Icons.format_paint,color: Colors.black,),),
                 )
               ],
             ),
@@ -44,7 +92,7 @@ class _PostPageState extends State<PostPage> {
                         Container(
 
                           child: CircleAvatar(
-                            backgroundColor: AppColor.WelcomeBackground,
+                            backgroundColor: colors[currentColor],
                             child: ClipOval(
                               child: Image.network(
                                 'https://support.signal.org/hc/article_attachments/360083910451/animated-2.gif',
@@ -61,7 +109,7 @@ class _PostPageState extends State<PostPage> {
                             width: 150,
                             height: 30,
                             decoration: BoxDecoration(
-                              color: AppColor.WelcomeBackground,
+                              color: colors[currentColor],
 
                               borderRadius:  BorderRadius.circular(50),
                             )
@@ -85,11 +133,11 @@ class _PostPageState extends State<PostPage> {
                     width: size.width,
                     height: size.height,
                     decoration: BoxDecoration(
-                      color: AppColor.WelcomeBackground,
+                      color: colors[currentColor],
 
                       borderRadius: const BorderRadius.only(
-                        topRight: Radius.circular(60),
-                        topLeft: Radius.circular(60),
+                        topRight: Radius.circular(40),
+                        topLeft: Radius.circular(40),
                       ),
                     ),
                   ),
@@ -118,96 +166,25 @@ class _PostPageState extends State<PostPage> {
                         ],
                       ),
                     ),
+                    SizedBox(height: 5,),
+
                     Column(
-                      children: [
-
-                        Container(
-
-                          margin: const EdgeInsets.only(top: 40,),
-                          height: 300,
-                          color: Colors.white,
-                          width: size.width,
-
-
-                          child:  Column(
-                            children: [
-                              Row(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.all(5.0),
-                                    child: CircleAvatar(
-                                      backgroundColor: AppColor.SecondMainColor,
-                                      child: ClipOval(
-                                        child: Image.network(
-                                          'https://support.signal.org/hc/article_attachments/360083910451/animated-2.gif',
-                                          fit: BoxFit.cover,
-                                        ),
-                                      ),
-                                      radius: 15,
-                                    ),
-                                  ),
-                                  const Text("User17",style: TextStyle(fontSize: 15,fontWeight: FontWeight.w600),),
-                                ],
-                              ),
-
-                              const Padding(
-                                  padding: EdgeInsets.symmetric(horizontal: 10),
-                                  child: Image(width: 450,
-                                      image: NetworkImage(
-                                          "https://www.oggusto.com/uploads/images/custom/15814-kategori.jpg"))
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
+                      children: posts.map((post) =>
+                          PostCard(
+                            post: post,
+                            delete: () {
+                              deletePost(post);
+                            },
+                            likeIncrement: () {
+                              LikeIncrement(post);
+                            },
+                          )).toList(),
                     ),
-                    Column(
-                      children: [
 
-                        Container(
-
-                          margin: const EdgeInsets.only(top: 40,),
-                          height: 320,
-                          color: Colors.white,
-                          width: size.width,
-
-
-                          child:  Column(
-                            children: [
-                              Row(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.all(5.0),
-                                    child: CircleAvatar(
-                                      backgroundColor: AppColor.SecondMainColor,
-                                      child: ClipOval(
-                                        child: Image.network(
-                                          'https://support.signal.org/hc/article_attachments/360083910451/animated-2.gif',
-                                          fit: BoxFit.cover,
-                                        ),
-                                      ),
-                                      radius: 15,
-                                    ),
-                                  ),
-                                  Text("User62",style: const TextStyle(fontSize: 15,fontWeight: FontWeight.w600),),
-                                ],
-                              ),
-
-                              const Padding(
-                                  padding: EdgeInsets.symmetric(horizontal: 10),
-                                  child: Image(width: 450,
-                                      image: NetworkImage(
-                                          "https://www.yabanihayvanlar.com/wp-content/uploads/2021/04/fil.jpg"))
-                              ),
-                            ],
-                          ),
-                        ),
-
-                      ],
-                    ),
 
                   ],
-                )),
+                )
+                ),
 
               ],
             )
