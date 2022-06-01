695b90ef8545c43b1bb1275a68487302656da5fd
alivehbi <101273047+alivehbi@users.noreply.github.com>
2022-06-01 13:36:15 +0300

    Update nft.dart

--- lib/Object Classes/nft.dart
+++ lib/Object Classes/nft.dart
@@ -1,7 +1,9 @@
 class NFT {
   String URL;
+  String UserURL;
   String text;
   String date;
+  String username;
   int likes;
   int money;
 
@@ -9,6 +11,8 @@ class NFT {
     required this.URL,
     required this.text,
     required this.date,
+    required this.UserURL,
+    required this.username,
     required this.likes,
     required this.money,
   });
